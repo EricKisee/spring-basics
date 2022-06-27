00356af016e63636324ca546ec64b9c86c228d83
EricKisee <erickiseemulwa@gmail.com>
2022-06-27 22:43:56 +0000

    spring java config

--- src/main/java/eric/Application.java
+++ src/main/java/eric/Application.java
@@ -1,5 +1,9 @@
 package eric;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import eric.config.ApplicationConfig;
 import eric.service.GreetingService;
 import eric.service.OutputService;
 import eric.service.TimeService;
@@ -7,9 +11,8 @@ import eric.service.TimeService;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
-        TimeService timeService = new TimeService(true);
-        OutputService outputService = new OutputService(greetingService, timeService);
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        OutputService outputService = context.getBean(OutputService.class);
 
         for (int i=0;i<5;i++){
             outputService.generateOutput("Frank");
