defc28655adb47f58a36872d64d4723945e617dd
EricKisee <erickiseemulwa@gmail.com>
2022-06-27 22:10:50 +0000

    Start

---
+++ src/main/java/eric/Application.java
@@ -0,0 +1,19 @@
+package eric;
+
+import eric.service.GreetingService;
+import eric.service.OutputService;
+import eric.service.TimeService;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        GreetingService greetingService = new GreetingService("Hello");
+        TimeService timeService = new TimeService(true);
+        OutputService outputService = new OutputService(greetingService, timeService);
+
+        for (int i=0;i<5;i++){
+            outputService.generateOutput("Frank");
+            Thread.sleep(5000);
+        }
+    }
+}
