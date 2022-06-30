3796c599fad9df05e30dfd43fc89d18439566bce
EricKisee <erickiseemulwa@gmail.com>
2022-06-30 07:46:51 +0000

    Creating an Aspect - Aspect Oriented Programming

--- src/main/java/eric/config/ApplicationConfig.java
+++ src/main/java/eric/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package eric.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "eric")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 }
